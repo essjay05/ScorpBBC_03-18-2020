7f6e40d24845518635595d8190f939a08f167bd1
essjay05 <stepjoyous@gmail.com>
2020-03-24 17:17:53 -0700

    First commit for weekly Boomerang Booty Challenge 03-18-2020

---
+++ AgeDiff/index.js
@@ -0,0 +1,5 @@
+// Challenge 1 of 3 (Easiest)
+// Given Array of all fam members ages (all whole numbers... 5 months = 0 ) in ANY order
+var famAges = [];
+
+// Return NEW array with [Youngest Age, Oldest Age, Age difference btw Oldest & youngest]
---
+++ AllOcc/index.js
@@ -0,0 +1,1 @@
+// Challenge 2 of 3 (Medium)
