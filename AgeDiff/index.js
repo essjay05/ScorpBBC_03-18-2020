261f9dc01535482099ba5371e8f7c0b7710de69a
essjay05 <stepjoyous@gmail.com>
2020-03-24 17:30:33 -0700

    Added directions for each of the challenges

--- AgeDiff/index.js
+++ AgeDiff/index.js
@@ -1,5 +1,10 @@
 // Challenge 1 of 3 (Easiest)
 // Given Array of all fam members ages (all whole numbers... 5 months = 0 ) in ANY order
-var famAges = [];
+var ages = [];
 
 // Return NEW array with [Youngest Age, Oldest Age, Age difference btw Oldest & youngest]
+
+function differenceInAges(ages) {
+    // Your code goes here
+}
+
--- PerfectPower/index.js
+++ PerfectPower/index.js
@@ -1,1 +1,28 @@
 // Challenge 3 of 3 (Hardest)
+
+// Perfect power is a classification of positive integers:
+    // In math, a perfect power is a positive integer that can be expressed as an integer power
+    // of another positive integer. More formally, n is a perfect power if there exist
+    // natural numbers m > 1, and k > 1 such that m to the K power = n.
+
+// Task: check whether a given integer is a perfect power.  If yes, return a pair
+    // m and k with m to the k power = n as proof.  Otherwise return Nothing, Nil, 
+    // null, NULL, None or your language's equivalent.
+
+// Note: for a perfect power, there might be several pairs.  For example 81= 3^4 =  9 ^2,
+    // so (3, 4) and (9, 2) are valid solutions.  However, the tests take care of this,
+    // so if a number is a perfect power, return any pair that proves it.
+    
+// Examples:
+    // Test.describe("perfect powers", function(){
+    //     Test.it("should work for some examples", function() {
+    //         Test.assertSimilar(isPP(4), [2,2], "4 = 2^2");
+    //         Test.assertSimilar(isPP(9), [3,2], "9 = 3^2");
+    //         Test.assertSimilar(isPP(5), null, "5 isn't a perfect number");
+    //     });
+    // });
+
+// Solution:
+var isPP = function(n) {
+    return null; //fix me
+}
